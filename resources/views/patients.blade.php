a7137fd28c25825cb07a6f7d14affd2dc2f5cb28
Loris <moine.loris420@gmail.com>
2025-09-12 18:06:27 +0200

    Changes on Patients View

--- resources/views/patients.blade.php
+++ resources/views/patients.blade.php
@@ -7,20 +7,26 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <h1>Liste des patients :</h1>
-    <section>
-        @foreach($patients as $patient)
-            <p>Nom : {{ $patient->nom }}</p>
-            <p>Prenom : {{ $patient->prenom }}</p>
-            <p>Date de naissance : {{ $patient->dateNaissance }}</p>
-            <p>Lieu de naissance : {{ $patient->lieuNaissance }}</p>
-            <p>Sexe : {{ $patient->sexe }}</p>
-            <p>Poids : {{ $patient->poids }}</p>
-            <p>Rue : {{ $patient->rue }}</p>
-            <p>Ville : {{ $patient->ville }}</p>
-            <p>Code postal : {{ $patient->codePostal }}</p>
-        @endforeach
+    <section class="sectionTitlePatient">
+        <h1>Liste des patients :</h1>
     </section>
+    <div class="containerGridPatient">
+        <section class="sectionGridPatient">
+            @foreach($patients as $patient)
+                <div class="blockInfoPatient">
+                    <p>Nom : {{ $patient->nom }}</p>
+                    <p>Prenom : {{ $patient->prenom }}</p>
+                    <p>Date de naissance : {{ $patient->dateNaissance }}</p>
+                    <p>Lieu de naissance : {{ $patient->lieuNaissance }}</p>
+                    <p>Sexe : {{ $patient->sexe }}</p>
+                    <p>Poids : {{ $patient->poids }}</p>
+                    <p>Rue : {{ $patient->rue }}</p>
+                    <p>Ville : {{ $patient->ville }}</p>
+                    <p>Code postal : {{ $patient->codePostal }}</p>
+                </div>
+            @endforeach
+        </section>
+    </div>
     <a href="{{ url('/') }}">Retour</a>
 </body>
 </html>
